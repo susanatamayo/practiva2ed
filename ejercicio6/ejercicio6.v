9e708f99dcf51bf18f7461ac7805f88694fc7f1f
Susana Tamayo <susana.tamayo@upb.edu.co>
2026-09-16 18:47:01 -0500

    Ejercicio 6: codigo, pines y link del video

---
+++ ejercicio6/ejercicio6.v
@@ -0,0 +1,8 @@
+//Descripción de flujo de datos de un sumador de 4 bits
+module _4bit_adder_df(iA, iB, iCin, oSUM, oCout);
+    input [3:0] iA, iB;
+    input iCin;
+    output [3:0] oSUM;
+    output oCout;
+    assign {oCout , oSUM} = iA + iB + iCin;
+endmodule
